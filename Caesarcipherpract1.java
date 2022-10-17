fd960d1659087d3b21d0f7339b8575985b9ca5e9
Chaitumulik <116012148+Chaitumulik@users.noreply.github.com>
2022-10-17 20:50:39 +0530

    Add files via upload

---
+++ Caesarcipherpract1.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package caesarcipherpract1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Chaitu
+ */
+public class Caesarcipherpract1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String message,encryptedmessage="";
+        int key;
+        char ch;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a message:");
+        message=sc.nextLine();
+        
+        System.out.println("Enter Key:");
+        key=sc.nextInt();
+        for (int i=0;i<message.length();i++)
+        {
+            ch=message.charAt(i);
+            
+            if(ch>='a'&&ch<='z')
+            {
+                ch=(char)(ch+key);
+                if(ch>'z'){ch=(char)(ch-'z'+'a'-1);}
+                encryptedmessage+=ch;
+            }
+            else if(ch>='A'&&ch<='z')
+            {
+                ch=(char)(ch+key);
+                if(ch>'z'){ch=(char)(ch-'z'+'A'-1);}
+                encryptedmessage+=ch;
+            }
+            else {encryptedmessage+=ch;}
+        }
+        System.out.println("Encrypted Message: "+encryptedmessage);
+    }
+    
+}
